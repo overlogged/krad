d45e53f810c14f3b0b1c10302ff4142b84006cb4
wispytrace <1025489007@qq.com>
2018-03-21 22:13:55 +0800

    the second

--- src/main/java/JavaMain.java
+++ src/main/java/JavaMain.java
@@ -1,3 +1,4 @@
+
 public class JavaMain {
     public static void main(String[] args){
         System.out.println("Hello Java");
--- src/main/scala/ScalaMain.scala
+++ src/main/scala/ScalaMain.scala
@@ -1,3 +1,4 @@
+
 object ScalaMain {
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
