3a87fab500c8fcb7f3471f9c55f7ccabee898302
Ignoramus0817 <Ignoramus0817@gmail.com>
2018-03-25 21:26:44 +0800

    add a class Player, modified GambleChecker&PlayerChecker

--- src/main/java/GambleChecker.java
+++ src/main/java/GambleChecker.java
@@ -1,3 +1,19 @@
 public class GambleChecker {
-    char[] checker;
+    //constant about judge
+    private static final int DRAW=0;
+    private static final int AWINS=1;
+    private static final int BWINS=2;
+
+    //judge the result of one pair of gamble
+    int win(Player playerA,Player playerB) {
+        switch (playerA.gamble - playerB.gamble) {
+            case 0:
+                return DRAW;
+            case 1:
+            case 2:
+                return AWINS;
+            default:
+                return BWINS;
+        }
+    }
 }
--- src/main/java/PlayerChecker.java
+++ src/main/java/PlayerChecker.java
@@ -1,2 +1,43 @@
-public class PlayerChecker {
+public class PlayerChecker{
+    //constant about teams
+    private static final int HUMAN=0;
+    private static final int ZOMBIE=1;
+
+    //element acquiring
+    void elemAcq(Player playerMain,int flag){
+        if(playerMain.hasElem==false)
+            playerMain.hasElem=true;
+    }
+
+    //element losing
+    void elemLose(Player playerMain){
+        if(playerMain.hasElem==true)
+            playerMain.hasElem=false;
+    }
+
+    //energy acquiring
+    void energyAcq(Player playerMain,int energyVal){
+        if(playerMain.energy<playerMain.energyLim)
+            playerMain.energy+=energyVal;
+    }
+
+    //energy consuming
+    void energyConsume(Player playerMain,int energyVal) {
+        if (playerMain.energy >= 0)
+            playerMain.energy -= energyVal;
+    }
+
+    void fire(Player playerA,Player playerB){
+        move(playerB);
+        playerA.energy=0;
+    }
+
+    void move(Player playerMain){}
+
+    void victory(Player playerMain){}
+
+    void infection(Player playerA,Player playerB){
+        if(playerA.team==ZOMBIE&playerB.team==HUMAN)
+            playerB.team=ZOMBIE;
+    }
 }
