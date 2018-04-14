3c9dadfd08bc4d6387521fe8d47b6462c1a4eace
wispytrace <1025489007@qq.com>
2018-04-14 23:16:32 +0800

    Add Serializable

--- src/main/java/MapChecker.java
+++ src/main/java/MapChecker.java
@@ -2,44 +2,90 @@
 import java.io.*;
 public class MapChecker {
 }
-class MapEdge{
+class MapEdge implements Serializable{
     int adjedg;   //The other unit sign
     int distance;  //The length of the edg
 }
-class Factor{      //The important thing in the map
+class Factor implements Serializable{      //The important thing in the map
     String name;
     void function(){}
 }
-class MapUnit{
-    int mark;     //The sign ofh a unit
-    MapEdge[] edg;   //The  connect edg
+class MapUnit implements Serializable{
+    int mark;       //The sign ofh a unit
+    int edgnumber;  //The number of edg
+    MapEdge[] edg;  // The  connect edg of unit
     int height;    //The height of the unit
     int is_factor;  //Judge the map status
-    Factor  key;    //Store the important thing
+    Factor  key=new Factor();    //Store the important thing
+    public void setEdg()
+    {
+        edg=new MapEdge[edgnumber];
+        for (int i=0;i<edgnumber;i++)
+        {
+            edg[i]=new MapEdge();
+        }
+    }
+
 }
 
-class SetMap{             //Set mapunit attribute to make up map
-    public static void SetMap(int number, MapEdge[][] edge_gather, int[] height_gather, int[] status_gather, Factor[] factor_gather){
-        MapUnit[] GameMap = new MapUnit[number];    //Set MapUnit object
-        for (int i=0; i<number; i++){
-            GameMap[i].mark = i;                    //Set mark number,from zero to number-1
-            GameMap[i].edg = edge_gather[i];     //Set connect
-            GameMap[i].height = height_gather[i];   //Set height
-            GameMap[i].is_factor = status_gather[i]; //Set factor
-            GameMap[i].key = factor_gather[i];
+class JavaMainTest{
+    public static void main(String[] args)throws Exception{
+        MapUnit[] sample=new MapUnit[12];
+        MapUnit[] show=new  MapUnit[12];
+        for (int i=0;i<12;i++)
+        {
+            sample[i] = new MapUnit();
+            sample[i].mark=i+1;
+            sample[i].height=i+1;
+            sample[i].is_factor=0;
+            sample[i].edgnumber=2;
+            if (i==6){sample[i].edgnumber=3;}
+            if (i==0){sample[i].edgnumber=1;}
+            sample[i].setEdg();
+            sample[i].edg[0].adjedg=i+2;
+            sample[i].edg[0].distance=1;
+            if (i!=0)
+            {
+                sample[i].edg[1].adjedg = i;
+                sample[i].edg[1].distance=1;
+            }
         }
+        sample[6].edg[2].adjedg=11;
+        sample[6].edg[2].distance=1;
+        sample[11].edg[0].adjedg=6;
+        sample[9].is_factor=1;
+        sample[9].key.name="Fate";
+        try
+        {
+            FileOutputStream fileOut = new FileOutputStream("C:\\Users\\欧阳炳濠\\Desktop\\employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            for (int i=0;i<12;i++)
+            {
+                out.writeObject(sample[i]);
+            }
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized data is saved in /tmp/employee.ser");
+        }catch(IOException i) {
+            i.printStackTrace();
+        }
+        FileInputStream fn = new FileInputStream("C:\\Users\\欧阳炳濠\\Desktop\\employee.ser");
+        ObjectInputStream ois = new ObjectInputStream(fn);
+        for (int i=0;fn.available()>0;i++)
+        {
+            show[i]=(MapUnit)ois.readObject();
+            System.out.print(show[i].mark);
+            System.out.print("  next is the ");
+            System.out.println(show[i].edg[0].adjedg);
+            System.out.print("the heigt of it is ");
+            System.out.println(show[i].height);
+            if ((show[i].is_factor|0)!=0) {
+                System.out.print("it has factor ");
+                System.out.println(show[i].key.name);
+            }
+        }
+
+
     }
 }
-class LoadFile{
-    public static  void LoadFile(String filepath) throws IOException{
-        try{
-            FileInputStream fis = new FileInputStream(filepath);
-            byte[] buffer= new byte[1024];
-            int count;
-            count = fis.read(buffer);
-        }
-        catch (Exception ioe){
-            System.out.println("File default");
-        }
-    }
-    }
+
