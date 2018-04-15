a37eb462e3e5f2fa94736bcf50514abd12ef7183
wispytrace <1025489007@qq.com>
2018-04-15 18:02:37 +0800

    Adjust format

---
+++ src/main/java/Factor.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+public class Factor implements Serializable {  //The important thing in the map
+        String name;
+        void  function(){
+        }
+}
--- src/main/java/MapChecker.java
+++ src/main/java/MapChecker.java
@@ -4,38 +4,8 @@ import java.io.*;
 public class MapChecker {
 }
 
-class MapEdge implements Serializable {
-    int adjedg;   //The other unit sign
-    int distance;  //The length of the edg
-}
 
-class Factor implements Serializable {      //The important thing in the map
-    String name;
 
-    void function() {
-    }
-}
-
-class MapUnit implements Serializable {
-    int mark;                    // The sign ofh a unit
-    int edgnumber;               // The number of edg
-    MapEdge[] edg;               // The connect edg of unit
-    int height;                  // The height of the unit
-    int is_factor;               // Judge the map status
-    Factor key = new Factor();   // Store the important thing
-
-    MapUnit() {
-    }
-
-    MapUnit(int number) {
-        edgnumber = number;
-        edg = new MapEdge[edgnumber];
-        for (int i = 0; i < edgnumber; i++) {
-            edg[i] = new MapEdge();   //edg[0] is the next,edg[1]is the before
-        }
-    }
-
-}
 
 class SetMap {           //It just a sample to make others test map
     public static void main(String[] args) throws Exception {
