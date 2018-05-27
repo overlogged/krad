102e99b8e5446ccbc56522cba9347064cfd06e29
nicekingwei <nicekingwei@foxmail.com>
2018-05-27 21:08:06 +0800

    add print stackTrace

--- src/main/scala/common/Mail.scala
+++ src/main/scala/common/Mail.scala
@@ -14,22 +14,23 @@ object Mail {
 
   /**
     * send an email
+    *
     * @return Future
     */
-  def send(to:String,subject:String,text:String) = Future {
-    val email = new HtmlEmail
-    email.setHostName(config.email_host)
-    email.setAuthentication(config.email_username,config.email_password)
-    email.addTo(to)
-    email.setFrom(config.email_username)
-    email.setSubject(subject)
-    email.setCharset("utf-8")
-    email.setHtmlMsg(text)
-    Server.log("[send]",to+subject+text)
-    try{
+  def send(to: String, subject: String, text: String) = Future {
+    try {
+      val email = new HtmlEmail
+      email.setHostName(config.email_host)
+      email.setAuthentication(config.email_username, config.email_password)
+      email.addTo(to)
+      email.setFrom(config.email_username)
+      email.setSubject(subject)
+      email.setCharset("utf-8")
+      email.setHtmlMsg(text)
+      Server.log("[send]", to + subject + text)
       email.send
-    }catch{
-      case e:EmailException => {
+    } catch {
+      case e: EmailException => {
         e.printStackTrace()
       }
     }
