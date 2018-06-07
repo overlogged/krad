fc7dad72cfb6e6b836627c44b8d5fa7509a51388
Ignoramus0817 <Ignoramus0817@gmail.com>
2018-06-07 17:47:43 +0800

    fixing console

--- html/assets/js/game.js
+++ html/assets/js/game.js
@@ -51,17 +51,14 @@ $("#ok").click(function(){
   var req = {};
 
   if(state=="Init"){
-  }else if(state=="Choose hero"){
+  }else if(state=="choose hero"){
     req = {
       hero:"hero"
     };
-  }else if(state=="Start game"){
+  }else if(state=="start game"){
   }else if(state=="choose strategy decision"){
     req = {
-      decision: parseInt(arg0)
-    }
-  }else if(state=="choose the feature of the decision"){
-    req = {
+      decision: parseInt(arg0), 
       moveDirection: parseInt(arg0),
       fireTarget: parseInt(arg1)
     }
@@ -72,6 +69,20 @@ $("#ok").click(function(){
   }else if(state=="GAMBLE:choose gamble"){
     req = {
       gambleCard:[parseInt(arg0)]
+    }
+  }else if(state=="win judge"){
+  }else if(state=="ACTION: deposit account"){
+  }else if(state=="skills account"){
+  }else if(state=="fire account"){
+  }else if(state=="move account"){
+  }else if(state=="element account"){
+  }else if(state=="if human wins"){
+  }else if(state=="Game Over, human wins"){
+  }else if(state=="infection account"){
+  }else if(state=="Game Over,zombie wins"){
+  }else if(state=="desert account"){
+    req = {
+      desertCardList:[parseIn(arg0)]
     }
   }
   request(req,do_nothing);
