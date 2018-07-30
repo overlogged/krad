7061164cf6cee57ae1914c14a706a3828246f672
Whydd <2998868901@qq.com>
2018-07-30 16:45:16 +0800

    change help_2

--- html/assets/js/login.js
+++ html/assets/js/login.js
@@ -22,15 +22,11 @@ var state_login = {
 
       game.add.button(580, 260, 'button', go_forget, this, 1, 0, 2, 0);
       game.add.bitmapText(600, 265, 'chiller', 'forget', 28);
-      game.add.button(500, 200, 'button', hh, this, 1, 0, 2, 0);
-
+     
 
 
       show("login");
   }
-}
-function hh(){
-  game.state.start('help');
 }
 
 function go_forget(){
