12b24b7c0303dacf94a5cbc920af49c04e91540a
Whydd <2998868901@qq.com>
2018-08-02 15:50:50 +0800

    add gif 9

--- html/assets/js/login.js
+++ html/assets/js/login.js
@@ -22,9 +22,9 @@ var state_login = {
 
       game.add.button(580, 260, 'button', go_forget, this, 1, 0, 2, 0);
       game.add.bitmapText(600, 265, 'chiller', 'forget', 28);
-     
 
 
+      
       show("login");
   }
 }
