86b210c62a8c6a9c33117eb3789f467870681321
nicekingwei <nicekingwei@foxmail.com>
2018-08-04 01:25:13 +0800

    change tryMove

--- src/main/java/game/MapChecker.java
+++ src/main/java/game/MapChecker.java
@@ -1,56 +1,52 @@
 package game;
 
+import server.Server;
+
 class MapChecker {
 
     static int tryMove(Map m, int i_current, int i_next, int energy) {
 
         // first step
-        MapUnit back = null;
-        MapUnit current = m.units[i_current];
-        MapUnit next = m.units[i_next];
+        MapUnit back,current,next;
 
-        boolean flag = false;
-        for (MapEdge e : current.edge) {
-            if (e.adjedg == i_next && energy >= e.distance) {
-                energy -= e.distance;
-                if (next.rank > current.rank) {
-                    energy = 0;
-                }
-                flag = true;
-                back = current;
-                current = next;
-                break;
-            }
-        }
-        assert flag;
+        int dis = m.distance[i_current][i_next];
+        if(energy>=dis){
+            energy-=dis;
+            back = m.units[i_current];
+            current = m.units[i_next];
+            if(current.rank>back.rank) energy = 0;
 
-        while (energy > 0) {
-            next = null;
-            int dis = 0;
+            while (energy > 0) {
+                next = null;
+                dis = 0;
 
-            // find next
-            for (MapEdge e : current.edge) {
-                if (back.mark != e.adjedg) {
-                    MapUnit try_next = m.units[e.adjedg];
-                    if (energy >= e.distance) {
-                        if (next == null || e.distance > dis) {
-                            next = try_next;
-                            dis = e.distance;
+                // find next
+                for (MapEdge e : current.edge) {
+                    if (back.mark != e.adjedg) {
+                        if (energy >= e.distance) {
+                            if (next == null || e.distance > dis) {
+                                next = m.units[e.adjedg];
+                                dis = e.distance;
+                            }
                         }
                     }
                 }
+
+                // do move
+                // assert next != null;
+                if(next==null) Server.log("error","tryMove 38");
+
+                energy -= dis;
+                if (next.rank > current.rank) {
+                    energy = 0;
+                }
+                back = current;
+                current = next;
             }
 
-            // do move
-            assert next != null;
-            energy -= dis;
-            if (next.rank > current.rank) {
-                energy = 0;
-            }
-            back = current;
-            current = next;
+        }else{
+            current = m.units[i_current];
         }
-
         return current.mark;
     }
 }
