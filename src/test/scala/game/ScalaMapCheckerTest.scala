abd64400110a82a2f7e07533356a83aec1a45cb7
nicekingwei <nicekingwei@foxmail.com>
2018-08-05 01:19:22 +0800

    refactor map

--- src/test/scala/game/ScalaMapCheckerTest.scala
+++ src/test/scala/game/ScalaMapCheckerTest.scala
@@ -2,17 +2,21 @@ package game
 
 object ScalaMapCheckerTest {
   def main(args: Array[String]): Unit = {
-    println(MapChecker.tryMove(new game.Map("map/1.map"),0,1,3))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),6,7,7))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),24,6,4))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),24,23,0))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),18,19,1))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),19,20,3))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),23,24,2))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),24,7,4))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),3,4,0))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),6,7,0))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),8,9,0))
-    println(MapChecker.tryMove(new game.Map("map/1.map"),6,7,3))
+    val map = new game.Map("map/1.map")
+    println(MapChecker.tryMove(map,0,1,3))
+    println(MapChecker.tryMove(map,6,7,7))
+    println(MapChecker.tryMove(map,24,6,4))
+    println(MapChecker.tryMove(map,24,23,0))
+    println(MapChecker.tryMove(map,18,19,1))
+    println(MapChecker.tryMove(map,19,20,3))
+    println(MapChecker.tryMove(map,23,24,2))
+    println(MapChecker.tryMove(map,24,7,4))
+    println(MapChecker.tryMove(map,3,4,0))
+    println(MapChecker.tryMove(map,6,7,0))
+    println(MapChecker.tryMove(map,8,9,0))
+    println(MapChecker.tryMove(map,6,7,3))
+    println(MapChecker.tryMove(map,4,map.units(4).neighbors(1),2))
+    println(MapChecker.tryMove(map,4,map.units(4).neighbors(1),1))
+    println(MapChecker.tryMove(map,4,map.units(4).neighbors(5),1))
   }
 }
