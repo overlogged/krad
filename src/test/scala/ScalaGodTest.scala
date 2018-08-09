7b273a4124a6f8eb8b9634c526902a1759ae00ec
nicekingwei <nicekingwei@foxmail.com>
2018-08-09 21:29:02 +0800

    modify test

--- src/test/scala/ScalaGodTest.scala
+++ src/test/scala/ScalaGodTest.scala
@@ -111,19 +111,6 @@ object ScalaGodTest extends MyJsonProtocol {
     }
   }
 
-  def test2(): Unit = {
-    SessionController.addGhost()
-    val fs = for (sid <- 0 to 1)
-      yield
-        for (data1 <- SessionController.gameRequest(RequestGame(sid, "{}"));
-             data2 <- SessionController.gameRequest(RequestGame(sid, "{\"hero\":\"hero\"}"));
-             data3 <- SessionController.gameRequest(RequestGame(sid, "{}"))) yield (data1, data2, data3)
-    for (f <- fs) {
-      f onComplete println
-      Await.ready(f, Duration.Inf)
-    }
-  }
-
   def test3(): Unit = {
     SessionController.addGhost()
     SessionController.gameRequest(RequestGame(2, "{}"))
@@ -132,78 +119,80 @@ object ScalaGodTest extends MyJsonProtocol {
 
   def test4(): Unit = {
     SessionController.addGhost()
-    for (_ <- SessionController.gameRequest(RequestGame(1, "{}")); // Wed Aug 08 22:25:38 CST 2018
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"hero\":\"Linear Algebra\"}")); // Wed Aug 08 22:25:43 CST 2018
-         _ <- SessionController.gameRequest(RequestGame(1, "{}")); // Wed Aug 08 22:25:46 CST 2018
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")); // Wed Aug 08 22:25:56 CST 2018
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"seenCard\":1}")); // Wed Aug 08 22:26:04 CST 2018
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"desertCardList\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"seenCard\":2}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"desertCardList\":[1,2,3,4,5]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"seenCard\":2}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"desertCardList\":[0,1,2,3,4]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"decision\":0,\"moveDirection\":1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"seenCard\":2}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"desertCardList\":[0,1,2]}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(1, "{\"seenCard\":1}"))
-    ){
-
+    val god = SessionController.states(1).god
+    val f1 = Future {
+      god.request(1, "{}") // Wed Aug 08 22:25:38 CST 2018
+      god.request(1, "{\"hero\":\"Linear Algebra\"}") // Wed Aug 08 22:25:43 CST 2018
+      god.request(1, "{}") // Wed Aug 08 22:25:46 CST 2018
+      god.request(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}") // Wed Aug 08 22:25:56 CST 2018
+      god.request(1, "{\"seenCard\":1}") // Wed Aug 08 22:26:04 CST 2018
+      god.request(1, "{\"gambleCard\":[-1]}")
+      god.request(1, "{}")
+      god.request(1, "{}")
+      god.request(1, "{\"desertCardList\":[-1]}")
+      god.request(1, "{}")
+      god.request(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(1, "{\"seenCard\":2}")
+      god.request(1, "{\"gambleCard\":[-1]}")
+      god.request(1, "{}")
+      god.request(1, "{}")
+      god.request(1, "{\"desertCardList\":[1,2,3,4,5]}")
+      god.request(1, "{}")
+      god.request(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(1, "{\"seenCard\":2}")
+      god.request(1, "{\"gambleCard\":[-1]}")
+      god.request(1, "{}")
+      god.request(1, "{}")
+      god.request(1, "{\"desertCardList\":[0,1,2,3,4]}")
+      god.request(1, "{}")
+      god.request(1, "{\"decision\":0,\"moveDirection\":1,\"fireTarget\":-1}")
+      god.request(1, "{\"seenCard\":2}")
+      god.request(1, "{\"gambleCard\":[-1]}")
+      god.request(1, "{}")
+      god.request(1, "{}")
+      god.request(1, "{\"desertCardList\":[0,1,2]}")
+      god.request(1, "{}")
+      god.request(1, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(1, "{\"seenCard\":1}")
     }
 
-    for (_ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"hero\":\"Calculus\"}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"seenCard\":1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"desertCardList\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"seenCard\":1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"desertCardList\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"seenCard\":1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"desertCardList\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"seenCard\":1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"gambleCard\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"desertCardList\":[-1]}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}"));
-         _ <- SessionController.gameRequest(RequestGame(2, "{\"seenCard\":1}"))
-    ){}
-    return ();
+    val f2 = Future {
+      god.request(2, "{}")
+      god.request(2, "{\"hero\":\"Calculus\"}")
+      god.request(2, "{}")
+      god.request(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(2, "{\"seenCard\":1}")
+      god.request(2, "{\"gambleCard\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{}")
+      god.request(2, "{\"desertCardList\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(2, "{\"seenCard\":1}")
+      god.request(2, "{\"gambleCard\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{}")
+      god.request(2, "{\"desertCardList\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(2, "{\"seenCard\":1}")
+      god.request(2, "{\"gambleCard\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{}")
+      god.request(2, "{\"desertCardList\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(2, "{\"seenCard\":1}")
+      god.request(2, "{\"gambleCard\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{}")
+      god.request(2, "{\"desertCardList\":[-1]}")
+      god.request(2, "{}")
+      god.request(2, "{\"decision\":-1,\"moveDirection\":-1,\"fireTarget\":-1}")
+      god.request(2, "{\"seenCard\":1}")
+    }
+    Await.result(f1,Duration.Inf)
+    Await.result(f2,Duration.Inf)
   }
 
   def main(args: Array[String]): Unit = {
